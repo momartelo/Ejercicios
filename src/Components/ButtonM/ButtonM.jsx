cbfcc558da074d16a44066e439989a3fcd1224ec
Martin <momartelo@yahoo.com.ar>
2025-10-28 19:45:01 -0300

    cambios

--- src/Components/ButtonM/ButtonM.jsx
+++ src/Components/ButtonM/ButtonM.jsx
@@ -1,4 +1,4 @@
-import styles from "./ButtonMui.module.css";
+import styles from "./ButtonM.module.css";
 import React from "react";
 import { Button, CircularProgress } from "@mui/material";
 
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,8 +1,5 @@
 import styles from "./HomePage.module.css";
 import MainLayout from "../../Layout/MainLayout";
-import ButtonMui from "../../Components/ButtonMuii/ButtonMui";
-import SaveIcon from "@mui/icons-material/Save";
-import Button from "../../Components/Button/Button";
 
 const HomePage = () => {
   return (
@@ -11,12 +8,6 @@ const HomePage = () => {
         <div className={styles.containerHome}>
           <h1>Home Page</h1>
           <h2>Curso React 2025</h2>
-          <ButtonMui text="prueba" startIcon={<SaveIcon />} color="success" />
-
-          <ButtonMui text="Cancelar" variant="outlined" color="error" />
-
-          <ButtonMui text="Enviar" />
-          <Button text="Guardar" />
         </div>
       </MainLayout>
     </>
